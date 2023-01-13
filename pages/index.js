d925489f0aaa242a7d5cb0df0d626a9fbb453724
alu0101015293 <72762373+alu0101015293@users.noreply.github.com>
2023-01-13 20:00:45 +0000

    Creamos el index con el pet.js perfect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-import Head from "next/head";
-import { useState } from "react";
+import Link from 'next/link'
 import styles from "./index.module.css";
 
-export default function Home() {
-  // Write the code
+function Home() {
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/homelogo.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/homelogo.png" className={styles.icon} />
+        <h2>Home</h2>
+        <h3>Choose where to go</h3>
+        <img src="/dog.png" className={styles.icon} />
+        <h4><Link href="/pet"> Pet name generator </Link></h4>
+        <br></br>
+        <img src="/cam.png" className={styles.icon} />
+        <h4><Link href="/image">Image Generator</Link></h4>
+        <br></br>
+      </main>
+    </div>
+  )
 }
+
+export default Home
