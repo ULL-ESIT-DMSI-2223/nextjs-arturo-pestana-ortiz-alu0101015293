b1937d40c0b07ada5c26427cc412ad18e9877d32
alu0101015293 <72762373+alu0101015293@users.noreply.github.com>
2023-01-13 18:09:42 +0000

    generate.js realizado

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,12 +17,14 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
+  console.log(capitalizeAnimal)
+  const suggestion = `Suggest three names for an animal that is a superhero.
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
 Names:`;
+  console.log(suggestion);
+  return suggestion;
 }
